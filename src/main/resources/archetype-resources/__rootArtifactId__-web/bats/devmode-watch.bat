9ad8e68d1db668c79a45a9038cd43a1f1f76f930
Tadeo Rivera <xtecuan@gmail.com>
2022-01-14 11:28:03 -0600

    Primera version estable+logo

--- src/main/resources/archetype-resources/__rootArtifactId__-web/bats/devmode-watch.bat
+++ src/main/resources/archetype-resources/__rootArtifactId__-web/bats/devmode-watch.bat
@@ -9,12 +9,4 @@ set MAVEN_OPTS="-Xms4G -Xmx10G -XX:MetaspaceSize=384M -XX:MaxMetaspaceSize=1024m
 set MAVEN_OPTS="%MAVEN_OPTS% -Djava.awt.headless=true -XX:+CompactStrings"
 echo %MAVEN_OPTS%
 
-REM set "exceltemplates=C:/Users/xtecuan/Documents/MINED/SGIPD/codigo/sgipd-main/sgipd-web/exceltemplates/"
-REM set "grade_folder=C:/Users/xtecuan/Documents/MINED/SGIPD/grades_xlsx/"
-REM  set "grade_folder_filters=departamento,municipio,centroeducativo"
-REM set "MSSQLSERVER_URL=jdbc:sqlserver://localhost:1433;databaseName=SGIDTPVD"
-REM set "MSSQLSERVER_PASSWORD=jup1t3r*2021"
-REM set "MSSQLSERVER_USER=JulianRivera"
-REM set "MSSQLSERVER_DATASOURCE=java:jboss/datasources/sgipdDS"
-
 %APP_HOME%\mvnw clean wildfly-jar:dev-watch
